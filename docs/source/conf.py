332c336b0363d0c05ae58781b5683b4faf18cb02
IvanRomero03 <i.wells.ar@gmail.com>
2022-09-19 18:28:55 -0500

    ext fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'renku.sphinx.theme'
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
